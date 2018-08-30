e7022c0dc67afb777ac58b243786ac85e271e75e
damooo <damodara@protonmail.com>
2018-08-30 08:11:36 +0530

    modified for repos

--- vedavaapi/ullekhanam/__init__.py
+++ vedavaapi/ullekhanam/__init__.py
@@ -10,57 +10,72 @@ import os
 
 # Essential for depickling to work.
 from sanskrit_data.schema import *  # pylint: disable=unused-import.
-from .backend import add_db
+from sanskrit_data.schema.books import BookPortion
+from sanskrit_data.schema.ullekhanam import TextAnnotation
+
 from vedavaapi.common import *
 
 logging.basicConfig(
-  level=logging.INFO,
-  format="%(levelname)s: %(asctime)s {%(filename)s:%(lineno)d}: %(message)s "
+    level=logging.INFO,
+    format="%(levelname)s: %(asctime)s {%(filename)s:%(lineno)d}: %(message)s "
 )
 
 # Dummy usage.
-#logging.debug("So that depickling works well, we imported: " + str([common, ullekhanam, books, users]))
+# logging.debug("So that depickling works well, we imported: " + str([common, ullekhanam, books, users]))
 
 from vedavaapi.common import VedavaapiService
 
 ServiceObj = None
 
+
 class VedavaapiUllekhanam(VedavaapiService):
-    dependency_services = ['store']
+    dependency_services = ['store', 'users']
 
     def __init__(self, registry, name, conf):
         super(VedavaapiUllekhanam, self).__init__(registry, name, conf)
         self.vvstore = VedavaapiServices.lookup("store")
         import_blueprints_after_service_is_ready(self)
-        
+
     def setup(self):
-        for db_details in self.config["ullekhanam_dbs"]:
-            add_db(db=self.vvstore.client.get_database_interface(db_name_backend=db_details["backend_id"],
-            db_name_frontend=db_details["frontend_id"],
-            external_file_store=db_details.get("file_store"), db_type="ullekhanam_db"), reimport=False)
+        db_name_end = self.config.get('ullekhanam_db')
+        file_store_base_path = self.config.get('file_store')
+        self.dbs_map = self.vvstore.db_interfaces_from_all_repos(
+            db_name_end=db_name_end,
+            db_name_frontend='ullekhanam',
+            file_store_base_path=file_store_base_path,
+            db_type="ullekhanam_db"
+        )
+        for repo, db in self.dbs_map.items():
+            BookPortion.add_indexes(db_interface=db)
+            TextAnnotation.add_indexes(db_interface=db)
+
 
     def reset(self):
-        for db_details in self.config["ullekhanam_dbs"]:
-            logging.info("Deleting database/collection " + db_details["backend_id"])
-            self.vvstore.client.delete_database(db_details["backend_id"])
-            if "file_store" in db_details:
-                try:
-                    os.system("rm -rf " + db_details["file_store"])
-                except Exception as e:
-                    logging.error("Error removing " + db_details["file_store"]+": "+ e)
+        db_name_end = self.config.get('ullekhanam_db')
+        file_store_base_path = self.config.get('file_store')
+        self.vvstore.delete_db_in_all_repos(
+            db_name_end=db_name_end,
+            delete_external_file_store=True,
+            file_store_base_path=file_store_base_path
+        )
+
+    def get_db(self, repo_id):
+        return self.dbs_map.get(repo_id, None)
+
 
 def myservice():
     return ServiceObj
 
+
 def get_store():
     return myservice().vvstore
 
+
 api_blueprints = []
+
 
 def import_blueprints_after_service_is_ready(service_obj):
     global ServiceObj
     ServiceObj = service_obj
     from .api_v1 import api_blueprint as apiv1_blueprint
     api_blueprints.append(apiv1_blueprint)
-
-
